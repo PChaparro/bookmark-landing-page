b69c15e93c73b866f8c3a0f672b60913d2c06281
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-06 14:19:01 -0500

    feat: [WIP] Initial responsive styles for the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import Styles from "./Navbar.module.css";
 import NavItem from "./NavItem/NavItem";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 const NavItems = [
   {
@@ -19,21 +21,87 @@ const NavItems = [
 ];
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleToggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <nav className={Styles.navbar__container}>
-      <Image
-        src="/images/logo-bookmark.svg"
-        alt="Bookmark logo"
-        className={Styles.logo}
-        width={148}
-        height={25}
-      />
-      <ul className={Styles.navbar__links}>
-        {NavItems.map((item, index) => (
-          <NavItem key={`navitem-${index}`} {...item} />
-        ))}
+      {/* Only show the dark logo if the mobile navigation is closed */}
+      {!isMenuOpen && (
+        <Image
+          src="/images/logo-bookmark.svg"
+          alt="Bookmark logo"
+          width={148}
+          height={25}
+        />
+      )}
+
+      <div
+        className={`${Styles["navbar__mobile-wrapper"]} ${
+          !!isMenuOpen && Styles["navbar__mobile-wrapper--active"]
+        }`}
+      >
+        {/* Mobile navigation header */}
+        <div className={Styles["navbar__mobile-header"]}>
+          <Image
+            src="/images/logo-bookmark-light.svg"
+            alt="Bookmark logo"
+            className={Styles["navbar__logo--light"]}
+            width={148}
+            height={25}
+          />
+          <button className={Styles["navbar__toggle-icon"]}>
+            <Image
+              src="/icons/icon-close.svg"
+              alt="Close menu icon"
+              width={18}
+              height={18}
+              onClick={handleToggleMenu}
+            />
+          </button>
+        </div>
+
+        {/* Shared navigation links */}
+        <ul className={Styles.navbar__links}>
+          {NavItems.map((item, index) => (
+            <NavItem key={`navitem-${index}`} {...item} />
+          ))}
+        </ul>
         <Button type="anchor" style="secondary" label="Login" />
-      </ul>
+
+        {/* Social media icons*/}
+        <div className={Styles.navbar__socials}>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/icons/icon-facebook.svg"
+              alt="Facebook icon"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/icons/icon-twitter.svg"
+              alt="Twitter icon"
+              width={24}
+              height={24}
+            />
+          </a>
+        </div>
+      </div>
+
+      {/* Show the hamburger icon is the mobile menu is hidden */}
+      {!isMenuOpen && (
+        <button className={Styles["navbar__toggle-icon"]}>
+          <Image
+            src="/icons/icon-hamburger.svg"
+            alt="Hamburger menu icon"
+            width={24}
+            height={18}
+            onClick={handleToggleMenu}
+          />
+        </button>
+      )}
     </nav>
   );
 }
